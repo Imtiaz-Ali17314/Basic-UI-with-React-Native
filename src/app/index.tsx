f0795a39d601536183212ce6e19c55f285669157
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-22 15:10:07 +0500

    feat: optimize task filtering logic using useMemo for improved performance

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import DateSelector from "../../components/DateSelector";
@@ -12,11 +12,19 @@ import { FilterOptions, TASKS } from "../../constants/tasks";
 export default function Index() {
   const insets = useSafeAreaInsets();
   const [activeFilter, setActiveFilter] = useState<FilterOptions>("All");
+
+  const filteredTasks = useMemo(() => {
+    if (activeFilter === "All") {
+      return TASKS;
+    }
+    return TASKS.filter((task) => task.status === activeFilter);
+  }, [activeFilter]);
+
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
       <StatusBar style="light" />
       <FlatList
-        data={TASKS}
+        data={filteredTasks}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <TaskCard tasks={item} />}
         ListHeaderComponent={
